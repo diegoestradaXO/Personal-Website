618b6d8c088e8cbc0807d2d9604adbb8b7894c78
Diego Estrada <est.diego03@gmail.com>
2020-06-04 02:47:53 -0600

    finished

---
+++ build/precache-manifest.d63bc381f16960b304113c3a64b99763.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c78c5f8e8d4c2fa0150807a32de5009",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d2aa117c9f77aa0b5527",
+    "url": "./static/css/main.82cdfca9.chunk.css"
+  },
+  {
+    "revision": "d727ada31ee55a1d3a56",
+    "url": "./static/js/2.fbd71057.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.fbd71057.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2aa117c9f77aa0b5527",
+    "url": "./static/js/main.a58651b1.chunk.js"
+  },
+  {
+    "revision": "7304c6cd1204852fb3fb",
+    "url": "./static/js/runtime-main.1451e3c5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "82bdc409965b4317c52fbe666f190fa8",
+    "url": "./static/media/present.82bdc409.jpg"
+  }
+]);
